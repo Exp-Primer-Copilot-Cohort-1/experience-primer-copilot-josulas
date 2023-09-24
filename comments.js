2202db54ad6b2cdc659f4dafb759a6dd6fb30b41
Josue F Laszeski <jlaszeski@itba.edu.ar>
2023-09-24 00:04:16 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// create web server
+// 1. create web server
+// 2. create db connection
+// 3. create schema
+// 4. create model
+// 5. create routes
+// 6. create controller
+// 7. create view
+// 8. create static files
+// 9. run web server
